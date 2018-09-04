fd7471c06f491c3a376eba24cf2b922ded70e17b
baggiowangyu <baggiowangyu@gmail.com>
2018-09-04 18:03:22 +0800

    调整项目结构，完成信息读取，增加日志模块

--- bgFramework/bgBase/bgAuthentication.cpp
+++ bgFramework/bgBase/bgAuthentication.cpp
@@ -1,6 +1,12 @@
 #include "bgAuthentication.h"
 #include "bgBase.h"
+#include <tchar.h>
+#include <atlstr.h>
 #include <Windows.h>
+
+#include "Poco/Net/NTPClient.h"
+#include "Poco/DigestEngine.h"
+#include "Poco/SHA1Engine.h"
 
 bgAuthentication authenticater;
 
@@ -71,36 +77,259 @@ std::string bgAuthentication::GetMachineCode()
 	std::string ret;
 
 	// 获取CPU信息
+	std::string processor_identifier = GetProcessorIdentifier();
+	std::string processor_name = GetProcessorProcessorNameString();
+	std::string processor_vendor_identifier = GetProcessorVendorIdentifier();
+
+	// 获取BIOS信息
+	std::string bios_vendor = GetBIOSVendor();
+	std::string bios_version = GetBIOSVersion();
 
 	// 获取硬盘信息（如果发现可插拔设备，直接不允许使用）
-
-	// 获取网卡信息
+	std::string disk_identifier = GetDiskIdentifier();
 
 	// SHA-256散列
+	std::string all_info = processor_identifier;
+	all_info += processor_name;
+	all_info += processor_vendor_identifier;
+	all_info += bios_vendor;
+	all_info += bios_version;
+	all_info += disk_identifier;
 
-	// Base64字符化
+	Poco::SHA1Engine sha1_engine;
+	sha1_engine.updateImpl(all_info.data(), all_info.length());
+	Poco::DigestEngine::Digest digist = sha1_engine.digest();
+	ret = Poco::DigestEngine::digestToHex(digist);
 
 	return ret;
 }
 
 std::string bgAuthentication::GetAuthenticateInfo()
 {
-	std::string ret;
+	int errCode = 0;
+	std::string ret = "";
 
 	// 从数据库读取数据
 	HKEY hKey = NULL;
 	DWORD dwDisposition = 0;
-	LONG lret = RegCreateKeyEx(HKEY_LOCAL_MACHINE, _T("SOFTWARE\\LRHW"), 0, NULL, 0, KEY_READ, NULL, &hKey, &dwDisposition);
+	LONG lret = RegCreateKeyExA(HKEY_LOCAL_MACHINE, "SOFTWARE\\LRHW", 0, NULL, 0, KEY_READ, NULL, &hKey, &dwDisposition);
 	if (lret != ERROR_SUCCESS)
 	{
+		errCode = GetLastError();
+		return ret;
 	}
 
+	DWORD dwType = 0;
+	char tszValue[4096] = {0};
+	DWORD dwValueLen = 4096;
+	lret = RegQueryValueExA(hKey, "AU_CODE", NULL, &dwType, (LPBYTE)tszValue, &dwValueLen);
+	errCode = GetLastError();
+
+	RegCloseKey(hKey);
+	hKey = NULL;
+
+	if (lret != ERROR_SUCCESS)
+	{
+		return ret;
+	}
+
+	ret = tszValue;
 	return ret;
 }
 
 bool bgAuthentication::SetAuthenticateInfo(std::string authen_code)
 {
+	int errCode = 0;
 	bool ret = false;
 
+	// 从数据库读取数据
+	HKEY hKey = NULL;
+	DWORD dwDisposition = 0;
+	LONG lret = RegCreateKeyExA(HKEY_LOCAL_MACHINE, _T("SOFTWARE\\LRHW"), 0, NULL, 0, KEY_READ|KEY_WRITE, NULL, &hKey, &dwDisposition);
+	if (lret != ERROR_SUCCESS)
+	{
+		errCode = GetLastError();
+		return false;
+	}
+
+	DWORD dwType = REG_SZ;
+	TCHAR tszValue[4096] = {0};
+	DWORD dwValueLen = 4096;
+	lret = RegSetValueExA(hKey, "AU_CODE", NULL, &dwType, (LPBYTE)authen_code.c_str(), authen_code.size() + 1);
+	errCode = GetLastError();
+
+	RegCloseKey(hKey);
+	hKey = NULL;
+
+	if (lret != ERROR_SUCCESS)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+std::string bgAuthentication::GetProcessorIdentifier()
+{
+	int errCode = 0;
+	std::string ret = "";
+
+	// 从数据库读取数据
+	HKEY hKey = NULL;
+	DWORD dwDisposition = 0;
+	LONG lret = RegCreateKeyExA(HKEY_LOCAL_MACHINE, "HARDWARE\\DESCRIPTION\\CentralProcessor\\0", 0, NULL, 0, KEY_READ, NULL, &hKey, &dwDisposition);
+	if (lret != ERROR_SUCCESS)
+	{
+		errCode = GetLastError();
+		return ret;
+	}
+
+	DWORD dwType = 0;
+	char tszValue[4096] = {0};
+	DWORD dwValueLen = 4096;
+	lret = RegQueryValueExA(hKey, "Identifier", NULL, &dwType, (LPBYTE)tszValue, &dwValueLen);
+	errCode = GetLastError();
+
+	RegCloseKey(hKey);
+	hKey = NULL;
+
+	if (lret != ERROR_SUCCESS)
+	{
+		return ret;
+	}
+
+	ret = tszValue;
+	return ret;
+}
+
+std::string bgAuthentication::GetProcessorProcessorNameString()
+{
+	int errCode = 0;
+	std::string ret = "";
+
+	// 从数据库读取数据
+	HKEY hKey = NULL;
+	DWORD dwDisposition = 0;
+	LONG lret = RegCreateKeyExA(HKEY_LOCAL_MACHINE, "HARDWARE\\DESCRIPTION\\CentralProcessor\\0", 0, NULL, 0, KEY_READ, NULL, &hKey, &dwDisposition);
+	if (lret != ERROR_SUCCESS)
+	{
+		errCode = GetLastError();
+		return ret;
+	}
+
+	DWORD dwType = 0;
+	char tszValue[4096] = {0};
+	DWORD dwValueLen = 4096;
+	lret = RegQueryValueExA(hKey, "ProcessorNameString", NULL, &dwType, (LPBYTE)tszValue, &dwValueLen);
+	errCode = GetLastError();
+
+	RegCloseKey(hKey);
+	hKey = NULL;
+
+	if (lret != ERROR_SUCCESS)
+	{
+		return ret;
+	}
+
+	ret = tszValue;
+	return ret;
+}
+
+std::string bgAuthentication::GetProcessorVendorIdentifier()
+{
+	int errCode = 0;
+	std::string ret = "";
+
+	// 从数据库读取数据
+	HKEY hKey = NULL;
+	DWORD dwDisposition = 0;
+	LONG lret = RegCreateKeyExA(HKEY_LOCAL_MACHINE, "HARDWARE\\DESCRIPTION\\CentralProcessor\\0", 0, NULL, 0, KEY_READ, NULL, &hKey, &dwDisposition);
+	if (lret != ERROR_SUCCESS)
+	{
+		errCode = GetLastError();
+		return ret;
+	}
+
+	DWORD dwType = 0;
+	char tszValue[4096] = {0};
+	DWORD dwValueLen = 4096;
+	lret = RegQueryValueExA(hKey, "VendorIdentifier", NULL, &dwType, (LPBYTE)tszValue, &dwValueLen);
+	errCode = GetLastError();
+
+	RegCloseKey(hKey);
+	hKey = NULL;
+
+	if (lret != ERROR_SUCCESS)
+	{
+		return ret;
+	}
+
+	ret = tszValue;
+	return ret;
+}
+
+std::string bgAuthentication::GetBIOSVendor()
+{
+	int errCode = 0;
+	std::string ret = "";
+
+	// 从数据库读取数据
+	HKEY hKey = NULL;
+	DWORD dwDisposition = 0;
+	LONG lret = RegCreateKeyExA(HKEY_LOCAL_MACHINE, "HARDWARE\\DESCRIPTION\\BIOS", 0, NULL, 0, KEY_READ, NULL, &hKey, &dwDisposition);
+	if (lret != ERROR_SUCCESS)
+	{
+		errCode = GetLastError();
+		return ret;
+	}
+
+	DWORD dwType = 0;
+	char tszValue[4096] = {0};
+	DWORD dwValueLen = 4096;
+	lret = RegQueryValueExA(hKey, "BIOSVendor", NULL, &dwType, (LPBYTE)tszValue, &dwValueLen);
+	errCode = GetLastError();
+
+	RegCloseKey(hKey);
+	hKey = NULL;
+
+	if (lret != ERROR_SUCCESS)
+	{
+		return ret;
+	}
+
+	ret = tszValue;
+	return ret;
+}
+
+std::string bgAuthentication::GetBIOSVersion()
+{
+	int errCode = 0;
+	std::string ret = "";
+
+	// 从数据库读取数据
+	HKEY hKey = NULL;
+	DWORD dwDisposition = 0;
+	LONG lret = RegCreateKeyExA(HKEY_LOCAL_MACHINE, "HARDWARE\\DESCRIPTION\\BIOS", 0, NULL, 0, KEY_READ, NULL, &hKey, &dwDisposition);
+	if (lret != ERROR_SUCCESS)
+	{
+		errCode = GetLastError();
+		return ret;
+	}
+
+	DWORD dwType = 0;
+	char tszValue[4096] = {0};
+	DWORD dwValueLen = 4096;
+	lret = RegQueryValueExA(hKey, "BIOSVersion", NULL, &dwType, (LPBYTE)tszValue, &dwValueLen);
+	errCode = GetLastError();
+
+	RegCloseKey(hKey);
+	hKey = NULL;
+
+	if (lret != ERROR_SUCCESS)
+	{
+		return ret;
+	}
+
+	ret = tszValue;
 	return ret;
 }
